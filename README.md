6ea681a7ff2a3617b28f4d32490ea6c4d6aafe7d
cherrynoize <cherrynoize9987@outlook.com>
2023-11-14 05:15:35 +0100

    added README

---
+++ README.md
@@ -0,0 +1,31 @@
+# irnm - improved rofi-network-manager
+## rofi interface utility for NetworkManager
+
+### Dependencies
+
+```
+sudo pacman -S bash
+yay -S rofi dunstify
+```
+
+### Installation
+
+```
+git clone https://github.com/cherrynoize/irnm irnm
+cd irnm
+chmod +x install.sh
+./install.sh
+```
+
+### Usage
+
+```
+irnm
+```
+
+### Screenshots
+
+![screenshot](screenshots/0.png "wpgtk theme")
+![screenshot](screenshots/1.png "wpgtk theme")
+![screenshot](screenshots/2.png "nord theme")
+![screenshot](screenshots/3.png "wpgtk theme")
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+PROGRAM_NAME="irnm"
+INSTALL_DIR="/usr/local/bin"
+DEST_FILE="$INSTALL_DIR/$PROGRAM_NAME"
+BAK_FILE="$DEST_FILE"
+TMP_LINK="$PROGRAM_NAME.ln"
+
+echo "[*] Installing $PROGRAM_NAME..."
+
+# cd to install directory
+cd "$(eval 'dirname "$(readlink -f "$0")"')" || exit 1
+# make program executable
+chmod +x "$PROGRAM_NAME"
+
+# find available file name for backup
+while [ -e "$BAK_FILE" ]; do
+  BAK_FILE="$BAK_FILE.bak"
+done
+
+# make backup if dest file exists and is not the same file
+if [ -e "$DEST_FILE" ] && ! diff "$PROGRAM_NAME" "$BAK_FILE"; then
+  echo "[*] Creating backup copy..."
+  sudo mv "$DEST_FILE" "$BAK_FILE" || exit 1
+  echo "Backup copy created: $BAK_FILE"
+fi
+
+# copy to install dir
+sudo cp "$PROGRAM_NAME" "$DEST_FILE"
+
+if [ -x "$DEST_FILE" ]; then
+  echo "[*] Install complete!"
+else
+  echo "[!] error: failed to install $PROGRAM_NAME"
+  exit 2
+fi
