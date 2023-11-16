7d081ca23774f005488855ad035969ba9029db64
cherrynoize <cherrynoize9987@outlook.com>
2023-11-17 00:53:24 +0100

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+# irnm - install script
+#############################
+# install config
 PROGRAM_NAME="irnm"
 INSTALL_DIR="/usr/local/bin"
 DEST_FILE="$INSTALL_DIR/$PROGRAM_NAME"
+#############################
+# do not edit below this line
+#############################
 BAK_FILE="$DEST_FILE"
-TMP_LINK="$PROGRAM_NAME.ln"
+
+confirm () {
+  while true; do
+    read -n 1 -p "Do you want to create a backup? (Y/n) " ans
+    [ -n "$ans" ] && echo # if input wasn't a return, add newline
+    case "$ans" in
+      [Yy]* | "" ) return ;;
+      [Nn]* ) return 1 ;;
+      * ) echo "invalid option: $ans" ;;
+    esac
+  done
+}
 
 echo "[*] Installing $PROGRAM_NAME..."
 
@@ -21,10 +38,13 @@ done
 if [ -e "$DEST_FILE" ] &&
   ! diff "$PROGRAM_NAME" "$DEST_FILE" > /dev/null &&
   [ ! "$1" = "-n" ] &&
-  [ ! "$1" = "--nobak" ]; then
-  echo "[*] Creating backup copy..."
-  sudo mv "$DEST_FILE" "$BAK_FILE" || exit 1
-  echo "Backup copy created: $BAK_FILE"
+  [ ! "$1" = "--nobak" ]
+then
+  if confirm; then
+    echo "[*] Creating backup copy..."
+    sudo mv "$DEST_FILE" "$BAK_FILE" || exit 1
+    echo "Backup copy created: $BAK_FILE"
+  fi
 fi
 
 # copy to install dir
